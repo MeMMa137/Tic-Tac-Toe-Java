43b983f41b62a414f86171db7917b55081d4718a
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-17 10:05:00 +0200

    Finished RestartGame class, added Homepage

--- TicTacToe/src/tictactoe/Homepage.java
+++ TicTacToe/src/tictactoe/Homepage.java
@@ -1,5 +1,5 @@
-
 package tictactoe;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -10,59 +10,67 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-public class Restartgame implements ActionListener
+
+
+
+public class Homepage implements ActionListener
 {
+	
 	JFrame frame =new JFrame();
-	JButton ReStartGame = new JButton("RESTART");
+	JButton buttonStartGame = new JButton("START ");
 	JPanel title_panel =new JPanel();
 	JLabel textfield =new JLabel();
-	
-	Restartgame()
-	{	
+	Homepage()
+	{
+		
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setSize(800,800);
 	frame.getContentPane().setBackground(null);
 	frame.setLayout(new BorderLayout());
 	frame.setVisible(true);
 	
-	frame.add(ReStartGame);
+	frame.add(buttonStartGame);
 	frame.add(title_panel);
 	frame.add(textfield);
 	
-	ReStartGame.setBounds(350,450,100,70);
-	ReStartGame.addActionListener(this);
-	ReStartGame.setFocusable(false);
-	ReStartGame.setBackground(Color.GREEN);
+	buttonStartGame.setBounds(350,450,100,70);
+	buttonStartGame.addActionListener(this);
+	buttonStartGame.setFocusable(false);
+	buttonStartGame.setBackground(Color.GREEN);
 	
 	
 	title_panel.setLayout(new BorderLayout());
-	title_panel.setBounds(0,0,8000,100);
-	title_panel.setBackground(Color.RED);
+	title_panel.setBounds(0,0,9000,100);
+	title_panel.setBackground(Color.YELLOW);
+	
+	
+	
 	textfield.setBackground(Color.CYAN);
 	textfield.setForeground(Color.black);
-	textfield.setFont(new Font("Ink Free",Font.BOLD,65));
+	textfield.setFont(new Font("Ink Free",Font.BOLD,70));
 	textfield.setHorizontalAlignment(JLabel.CENTER);
-	textfield.setText("Do you wana play again Tic tac toe");
+	textfield.setText(" Welcom to our Game ");
 	textfield.setOpaque(true);
 	
 	}
-        
-        public static void main(String[] args) 
+	
+	public static void main(String[] args) 
 	{
-		Restartgame restartgame= new Restartgame();
+		Homepage homepage = new Homepage();
 		
 	}
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource()==ReStartGame) 
+		if(e.getSource()==buttonStartGame) 
 		{
 		    frame.dispose();
-		    //Homepage homepage = new Homepage();
 			TicTacToe tictactoe = new TicTacToe();
 			
 		}
 		
 		
 	}
-}
+
+	
+	}
